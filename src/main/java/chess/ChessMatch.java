62d6d0a99253af4be81bd11fc952dc736164872a
Gabriel Lisboa <gabriellisboa.rx@gmail.com>
2024-06-21 17:42:12 -0300

    feat: place pieces king and rook on the board

--- src/main/java/chess/ChessMatch.java
+++ src/main/java/chess/ChessMatch.java
@@ -1,22 +1,31 @@
 package chess;
 
 import board.Board;
+import board.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
     private Board board;
 
     public ChessMatch() {
-        this.board = new Board(8,8);
+        this.board = new Board(8, 8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces() {
         ChessPiece[][] matrix = new ChessPiece[board.getRows()][board.getColumns()];
-        for (int i=0; i < board.getRows(); i++) {
-            for (int j=0; j < board.getRows(); j++) {
+        for (int i = 0; i < board.getRows(); i++) {
+            for (int j = 0; j < board.getRows(); j++) {
                 matrix[i][j] = (ChessPiece) board.piece(i, j);
             }
         }
 
         return matrix;
     }
-;}
+
+    private void initialSetup() {
+        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
+        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+    };
+}
